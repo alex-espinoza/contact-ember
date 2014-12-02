285f4a7ae9c2d7c3384cb4e7c618044c51b78b1b
Alex Espinoza <aespinoza@aspria.net>
2014-12-02 10:28:28 -0500

    Create rails contact model and seed data

--- rails/db/seeds.rb
+++ rails/db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Contact.create(name: "Bugs Bunny")
+Contact.create(name: "Wile E. Coyote")
+Contact.create(name: "Yosemite Sam")
