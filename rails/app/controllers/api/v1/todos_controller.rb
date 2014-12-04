aa5d22e8850ab5279800568e419e76c41df0837b
Alex Espinoza <aespinoza@aspria.net>
2014-12-04 17:31:08 -0500

    Create ember POSTing functionality of new todos and rails API endpoints to create them

--- rails/app/controllers/api/v1/todos_controller.rb
+++ rails/app/controllers/api/v1/todos_controller.rb
@@ -6,4 +6,19 @@ class Api::V1::TodosController < ApplicationController
   def show
     render json: Todo.find(params[:id])
   end
+
+  def create
+    @todo = Todo.new(todo_params)
+
+    if @todo.valid? && @todo.save
+      @todo.save
+      render json: @todo, status: 201
+    end
+  end
+
+  private
+
+  def todo_params
+    params.require(:todo).permit(:title, :is_completed)
+  end
 end
