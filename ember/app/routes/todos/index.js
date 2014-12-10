eb8610f3f287a45ffc813585042fcb3854745e33
Alex Espinoza <aespinoza@aspria.net>
2014-12-10 11:40:13 -0500

    Remove unneeded renderTempate call in todos/index route

--- ember/app/routes/todos/index.js
+++ ember/app/routes/todos/index.js
@@ -3,9 +3,5 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function() {
     return this.modelFor('todos');
-  },
-
-  renderTemplate: function(controller) {
-    this.render('todos/index', {controller: controller});
   }
 });
