f83bf13b1cda33abee9e04680c8306388997b34e
Alex Espinoza <aespinoza@aspria.net>
2014-12-10 11:17:21 -0500

    I'm stupid and controllers don't need to be nested, remaining and inflection works again

--- ember/app/routes/todos/index.js
+++ ember/app/routes/todos/index.js
@@ -3,5 +3,9 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function() {
     return this.modelFor('todos');
+  },
+
+  renderTemplate: function(controller) {
+    this.render('todos/index', {controller: controller});
   }
 });
